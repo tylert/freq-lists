390d4cea6217682c3c49ee080f66d89406961615
Tyler Tidman <tyler.tidman@draak.ca>
2021-08-26 18:06:32 -0400

    New multi-outputter now obviates storing old data files (works with old chirp_channels script too)

--- multi_outputer.py
+++ multi_outputer.py
@@ -15,13 +15,43 @@ def output_chirp(channels, max_name_length=16):
         mode = channel['Mode']
 
         # Find out the duplex and offset
-        if channel['TxFrequencyOffset'] == '+0.0' \
-                or channel['TxFrequencyOffset'] is None \
-                or channel['TxFrequencyOffset'] == '':
+        if channel['TxFrequencyOffset'] is None \
+                or channel['TxFrequencyOffset'] == '' \
+                or channel['TxFrequencyOffset'] == '+0.0':
             duplex = ''
             offset = 0
 
-        print(f"{count},{name[:max_name_length]},{frequency:.6f},{duplex},{offset:.6f},,,,,,{mode}")
+        # Figure out a "good" TStep value
+        if frequency > 30:  # VHF and up
+            if frequency > 300:  # UHF and up
+                tstep = 6.25  # kHz
+            else:
+                tstep = 5.00  # kHz
+        # else:
+        #     tstep = 1.00  # kHz
+        # XXX FIXME TODO find out what tstep to use for HF
+
+        # Send CTCSS???
+        if 'CtcssDecode' in channel.keys():
+            if channel['CtcssDecode'] is None \
+                    or channel['CtcssDecode'] == '':
+                r_tone_freq = '88.5'
+        else:
+            r_tone_freq = '88.5'
+
+        # Expect CTCSS???
+        if 'CtcssEncode' in channel.keys():
+            if channel['CtcssEncode'] is not None \
+                    or channel['CtcssEncode'] == '':
+                c_tone_freq = '88.5'
+        else:
+            c_tone_freq = '88.5'
+
+        # XXX FIXME TODO Get DCS/DTCS stuff working!!!
+        dtcs_code = '023'
+        dtcs_polarity = 'NN'
+
+        print(f"{count},{name[:max_name_length]},{frequency:.6f},{duplex},{offset:.6f},,{r_tone_freq},{c_tone_freq},{dtcs_code},{dtcs_polarity},{mode},{tstep:.2f},,,,,,")
         count += 1
 
 
