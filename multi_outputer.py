71bf5b4d1b0b06af9f34e3421bacd7ac84f53a70
Tyler Tidman <tyler.tidman@draak.ca>
2021-08-28 22:21:38 -0400

    Fix a minor bug in the CTCSS output stuff for the new converter script

--- multi_outputer.py
+++ multi_outputer.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 
 
+# XXX FIXME TODO CSV output???
+# XXX FIXME TODO RT Systems output???
+
+
 from ruamel.yaml import YAML
 import click
 
@@ -10,7 +14,7 @@ def output_chirp_channels(channels, max_name_length=16):
 
     location = 1
     for channel in channels:
-        name = channel['Name'][:max_name_length]
+        name = channel['Name'][:max_name_length]  # XXX FIXME TODO Sanitize names!!!
         frequency = channel['RxFrequency']
         mode = channel['Mode']
 
@@ -28,18 +32,10 @@ def output_chirp_channels(channels, max_name_length=16):
             duplex = ''
             offset = 0
 
-        # Figure out a "good" TStep value
-        if frequency > 30:  # VHF and up
-            if frequency > 300:  # UHF and up
-                tstep = 6.25  # kHz
-            else:
-                tstep = 5.00  # kHz
-        else:
-            tstep = 5.00  # kHz
-
         # Offer CTCSS???
         if 'CtcssDecode' in channel.keys():
             if channel['CtcssDecode'] is None \
+                    or channel['CtcssDecode'] == 'None' \
                     or channel['CtcssDecode'] == '':
                 r_tone_freq = '88.5'
             else:
@@ -50,6 +46,7 @@ def output_chirp_channels(channels, max_name_length=16):
         # Demand CTCSS???
         if 'CtcssEncode' in channel.keys():
             if channel['CtcssEncode'] is None \
+                    or channel['CtcssEncode'] == 'None' \
                     or channel['CtcssEncode'] == '':
                 c_tone_freq = '88.5'
             else:
@@ -57,13 +54,26 @@ def output_chirp_channels(channels, max_name_length=16):
         else:
             c_tone_freq = '88.5'
 
+        # XXX FIXME TODO Get the tone field working properly!!!
+
         # XXX FIXME TODO Get DCS/DTCS stuff working!!!
         dtcs_code = '023'
         dtcs_polarity = 'NN'
 
+        # Figure out a "good" TStep value
+        if frequency > 30:  # VHF and up
+            if frequency > 300:  # UHF and up
+                tstep = 6.25
+            else:
+                tstep = 5.00
+        else:
+            tstep = 5.00
+
         print(f"{location},{name},{frequency:.6f},{duplex},{offset:.6f},,{r_tone_freq},{c_tone_freq},{dtcs_code},{dtcs_polarity},{mode},{tstep:.2f},,,,,,")
         location += 1
 
+
+# XXX FIXME TODO length switch
 
 @click.command()
 @click.option('--input_file', '-i', default=None, help='input')
