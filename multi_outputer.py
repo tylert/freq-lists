2db61c20e7f15a32de2fc0479c24e793f7eb7ee9
Tyler Tidman <tyler.tidman@draak.ca>
2021-08-30 14:48:10 -0400

    Get the tone values working properly for CHIRP

--- multi_outputer.py
+++ multi_outputer.py
@@ -9,12 +9,33 @@ from ruamel.yaml import YAML
 import click
 
 
-def output_chirp_channels(channels, max_name_length=16):
+def sanitize_channel_name(name, length=8):
+
+    # XXX FIXME TODO Maybe round up when truncating numerical channel names???
+
+    # If the name is already the right length, just use it
+    if len(name) <= length and length > 0:
+        return name
+    elif length < 0:
+        raise ValueError('Invalid name length!')
+
+    # Try to force the name to fix in the specified length and cull trailing whitespace
+    new_name = name[:length].strip()
+
+    # Try to force names to not have strange, chopped-off stuff at the end
+    if ' ' in new_name:
+        new_name = name[:length].strip().split()[0]
+
+    # Provive the sanitized name
+    return new_name
+
+
+def output_chirp_channels(channels, max_name_length=8):
     print('Location,Name,Frequency,Duplex,Offset,Tone,rToneFreq,cToneFreq,DtcsCode,DtcsPolarity,Mode,TStep,Skip,Comment,URCALL,RPT1CALL,RPT2CALL,DVCODE')
 
     location = 1
     for channel in channels:
-        name = channel['Name'][:max_name_length]  # XXX FIXME TODO Sanitize names!!!
+        name = sanitize_channel_name(channel['Name'], max_name_length)
         frequency = channel['RxFrequency']
         mode = channel['Mode']
 
@@ -32,29 +53,44 @@ def output_chirp_channels(channels, max_name_length=16):
             duplex = ''
             offset = 0
 
-        # Offer CTCSS???
+        # Send CTCSS tones
         if 'CtcssDecode' in channel.keys():
-            if channel['CtcssDecode'] is None \
-                    or channel['CtcssDecode'] == 'None' \
-                    or channel['CtcssDecode'] == '':
-                r_tone_freq = '88.5'
-            else:
+            if channel['CtcssDecode'] is not None \
+                    or channel['CtcssDecode'] != 'None' \
+                    or channel['CtcssDecode'] != '':
                 r_tone_freq = channel['CtcssDecode']
+            else:
+                r_tone_freq = '88.5'
         else:
             r_tone_freq = '88.5'
 
-        # Demand CTCSS???
+        # Listen for CTCSS tones
         if 'CtcssEncode' in channel.keys():
-            if channel['CtcssEncode'] is None \
-                    or channel['CtcssEncode'] == 'None' \
-                    or channel['CtcssEncode'] == '':
-                c_tone_freq = '88.5'
-            else:
+            if channel['CtcssEncode'] is not None \
+                    or channel['CtcssEncode'] != 'None' \
+                    or channel['CtcssEncode'] != '':
                 c_tone_freq = channel['CtcssEncode']
+            else:
+                c_tone_freq = '88.5'
         else:
             c_tone_freq = '88.5'
 
-        # XXX FIXME TODO Get the tone field working properly!!!
+        # CTCSS tone type
+        if 'CtcssDecode' in channel.keys() and 'CtcssEncode' in channel.keys():
+            if channel['CtcssDecode'] is not None and channel['CtcssEncode'] is not None \
+                    and channel['CtcssDecode'] != 'None' and channel['CtcssEncode'] != 'None' \
+                    and channel['CtcssDecode'] != '' and channel['CtcssEncode'] != '':
+                tone = 'TSQL'
+                c_tone_freq = r_tone_freq
+            elif channel['CtcssDecode'] is not None \
+                    and channel['CtcssDecode'] != 'None' \
+                    and channel['CtcssDecode'] != '':
+                tone = 'Tone'
+                c_tone_freq = r_tone_freq
+            else:
+                raise ValueError('Expecting a tone but not sending one!')
+        else:
+            tone = ''
 
         # XXX FIXME TODO Get DCS/DTCS stuff working!!!
         dtcs_code = '023'
@@ -69,20 +105,35 @@ def output_chirp_channels(channels, max_name_length=16):
         else:
             tstep = 5.00
 
-        print(f"{location},{name},{frequency:.6f},{duplex},{offset:.6f},,{r_tone_freq},{c_tone_freq},{dtcs_code},{dtcs_polarity},{mode},{tstep:.2f},,,,,,")
+        print(f"{location},{name},{frequency:.6f},{duplex},{offset:.6f},{tone},{r_tone_freq},{c_tone_freq},{dtcs_code},{dtcs_polarity},{mode},{tstep:.2f},,,,,,")
         location += 1
+
+
+def output_rt_systems_1_channels():
+    print('Receive Frequency,Transmit Frequency,Offset Frequency,Offset Direction,Repeater Use,Operating Mode,Name,Sub Name,Tone Mode,CTCSS,Rx CTCSS,DCS,DCS Polarity,Skip,Step,Digital Squelch,Digital Code,Your Callsign,Rpt-1 CallSign,Rpt-2 CallSign,LatLng,Latitude,Longitude,UTC Offset,Bank,Bank Channel Number,Comment')
+# 145.55,144.95,600 kHz,-DUP,,DV,D-Star VE3AAR C,,None,88.5 Hz,88.5 Hz,23,Both N,Off,10 kHz,Off,0,CQCQCQ,,,,,,, ,,
+# 145.55,144.95,600 kHz,-DUP,,DV,D-Star Union V,,None,88.5 Hz,88.5 Hz,23,Both N,Off,10 kHz,Off,0,CQCQCQ,,,,,,, ,,
+# 145.6,145.6, ,Simplex,,DV,145.600 Dstar Si,,None,88.5 Hz,88.5 Hz,23,Both N,Off,10 kHz,Off,0,,,,,,,, ,,
+# 145.62,145.62, ,Simplex,,DV,145.620 Dstar Si,,None,88.5 Hz,88.5 Hz,23,Both N,Off,10 kHz,Off,0,,,,,,,, ,,
+# 444.1,449.1,5.00 MHz,+DUP,,DV,VA3AAR B,,None,88.5 Hz,88.5 Hz,23,Both N,Off,25 kHz,Off,0,,,,,,,, ,,
+
+
+def output_rt_systems_2_channels():
+    print('Channel Number,Receive Frequency,Transmit Frequency,Offset Frequency,Offset Direction,Operating Mode,Name,Tone Mode,CTCSS,Rx CTCSS,DCS,DCS Polarity,Skip,Step,Bank,Bank Channel Number,Comment,Digital Squelch,Digital Code,Your Callsign,Rpt-1 CallSign,Rpt-2 CallSign')
+# 19,445.8000,445.8,000 kHz,Simplex,DV,445.800 Dstar Simplex,None,88.5 Hz,88.5 Hz,23,Both N,Off,10 kHz,A: ,,,Off,0,,,
 
 
 # XXX FIXME TODO length switch
 
 @click.command()
 @click.option('--input_file', '-i', default=None, help='input')
-def main(input_file):
+@click.option('--max_name_length', '-m', default=8, help='Maximum length of channel names (default 8).')
+def main(input_file, max_name_length):
     with open(input_file) as f:
         yaml = YAML(typ='safe')
         payload = yaml.load(f)
 
-    output_chirp_channels(payload['channels'], max_name_length=6)
+    output_chirp_channels(payload['channels'], max_name_length=max_name_length)
 
 
 if __name__ == '__main__':
