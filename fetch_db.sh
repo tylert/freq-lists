c1681ada05316fd4a880a40ce1dbe55fdac980a6
Tyler Tidman <tyler.tidman@draak.ca>
2022-03-19 14:55:40 -0400

    Fix up the db fetcher script a bit more

--- fetch_db.sh
+++ fetch_db.sh
@@ -13,9 +13,13 @@ mkdir -p tmp
 # CCS7 = Callsign Communications System 7
 #   https://sites.google.com/site/darathursdaynite/d-star/d-star-ccs7-whats-that
 
+# Retevis RT90 storage is limited to 100,000
+# Retevis RT3S storage is limited to 120,000
+
 # Download CCS7 DMR ID database and try to ensure it still fits in the radio
 # Force all rows to have 7 columns, sort by ID, drop lines that don't start with an ID
 wget --continue --output-document=tmp/user.csv https://database.radioid.net/static/user.csv
 cat tmp/user.csv | cut -d',' -f1-7 | sort -g | egrep '^[0-9]' > tmp/scrubbed.csv
-echo -e "Australia\nCanada\nNew Zealand\nUnited Kingdom\nUnited States" > tmp/countries.txt
+# echo -e "Canada\nUnited States" > tmp/countries.txt  # too big!!!
+echo -e "Australia\nCanada\nFrance\nUnited Kingdom\nNew Zealand" > tmp/countries.txt
 dmrRadio filterUsers tmp/countries.txt tmp/scrubbed.csv tmp/filtered.csv
