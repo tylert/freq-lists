c09b48c84de57a72bdb747d8ccfd393d8df64c75
Tyler Tidman <tyler.tidman@draak.ca>
2022-03-20 10:29:00 -0400

    Trim out some more GUOC and minify the DMR ID files more

--- fetch_db.sh
+++ fetch_db.sh
@@ -21,7 +21,7 @@ mkdir -p tmp
 # Download CCS7 DMR ID database and try to ensure it still fits in the radio
 # Force all rows to have 7 columns, sort by ID, drop lines that don't start with an ID
 wget --continue --output-document=tmp/user.csv https://database.radioid.net/static/user.csv
-cat tmp/user.csv | cut -d',' -f1-7 | sort -g | egrep '^[0-9]' > tmp/scrubbed.csv
+cut -d',' -f1-7 tmp/user.csv | sort -g | egrep '^[0-9]' > tmp/scrubbed.csv
 # echo -e "Canada\nUnited States" > tmp/countries.txt  # too big!!!
-echo -e "Australia\nCanada\nFrance\nUnited Kingdom\nNew Zealand" > tmp/countries.txt
+printf 'Australia\nCanada\nFrance\nUnited Kingdom\nNew Zealand\n' > tmp/countries.txt
 dmrRadio filterUsers tmp/countries.txt tmp/scrubbed.csv tmp/filtered.csv
