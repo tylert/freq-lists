737c66d59b3b480bac8df9a22c608350b338c578
Tyler Tidman <tyler.tidman@draak.ca>
2022-04-02 13:41:36 -0400

    Prepare to start combining data files more

--- generate_csv.sh
+++ generate_csv.sh
@@ -15,11 +15,6 @@ info/Packet_FM_VHF_UHF.yaml
 subtastic/Simplex_FM_VHF.yaml
 subtastic/Simplex_FM_UHF.yaml
 subtastic/Lanark_County_VHF.yaml
-'
-
-# Append a few files to the handout sheet that aren't in the other outputs
-human_input_files=${input_files}
-human_input_files+='
 repeaters/Lanark_DSTAR.yaml
 repeaters/Ottawa_DSTAR.yaml
 repeaters/Renfrew_DSTAR.yaml
@@ -45,9 +40,11 @@ index=1
 for input_file in ${input_files}; do
     if [[ 1 == ${index} ]]; then
         ./multi_outputter.py --format CHIRP --input_file ${input_file} \
+            --max_name_length 8 --only_modes AM,FM,NFM \
             --start_index 1 > ${chirp_output_file}
     else
         ./multi_outputter.py --format CHIRP --input_file ${input_file} \
+            --max_name_length 8 --only_modes AM,FM,NFM \
             --start_index $(wc -l ${chirp_output_file} | cut -d' ' -f1) \
             | tail -n '+2' >> ${chirp_output_file}
     fi
@@ -65,9 +62,11 @@ index=1
 for input_file in ${input_files}; do
     if [[ 1 == ${index} ]]; then
         ./multi_outputter.py --format RT --input_file ${input_file} \
+            --max_name_length 8 --only_modes AM,FM,NFM \
             > ${rt_systems_output_file}
     else
         ./multi_outputter.py --format RT --input_file ${input_file} \
+            --max_name_length 8 --only_modes AM,FM,NFM \
             | tail -n '+2' >> ${rt_systems_output_file}
     fi
     index=$((${index} + 1))
@@ -83,12 +82,27 @@ done
 # XXX FIXME TODO  https://openpyxl.readthedocs.io/en/stable/styles.html#edit-page-setup
 
 index=1
-for input_file in ${human_input_files}; do
+for input_file in ${input_files}; do
     if [[ 1 == ${index} ]]; then
         ./multi_outputter.py --format HUMAN --input_file ${input_file} \
+            --max_name_length 8 --only_modes AM,FM,NFM \
             --start_index 1 > ${human_output_file}
     else
         ./multi_outputter.py --format HUMAN --input_file ${input_file} \
+            --max_name_length 8 --only_modes AM,FM,NFM \
+            --start_index $(wc -l ${human_output_file} | cut -d' ' -f1) \
+            | tail -n '+2' >> ${human_output_file}
+    fi
+    index=$((${index} + 1))
+done
+for input_file in ${input_files}; do
+    if [[ 1 == ${index} ]]; then
+        ./multi_outputter.py --format HUMAN --input_file ${input_file} \
+            --max_name_length 8 --only_modes DSTAR,DMR,YSF \
+            --start_index 1 > ${human_output_file}
+    else
+        ./multi_outputter.py --format HUMAN --input_file ${input_file} \
+            --max_name_length 8 --only_modes DSTAR,DMR,YSF \
             --start_index $(wc -l ${human_output_file} | cut -d' ' -f1) \
             | tail -n '+2' >> ${human_output_file}
     fi
