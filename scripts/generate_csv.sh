798d803ca8f27ada77cd33498c6cc43843d72b5a
Tyler Tidman <tyler.tidman@draak.ca>
2022-05-10 19:44:52 -0400

    Ensure the RDT files generated also have date stamps

--- scripts/generate_csv.sh
+++ scripts/generate_csv.sh
@@ -5,6 +5,7 @@
 
 # set -x
 mkdir -p tmp
+date="$(date +%Y-%m-%d)"
 
 input_files='
 repeaters/Lanark_AARC.yaml
@@ -25,10 +26,10 @@ subtastic/Simplex_FM.yaml
 subtastic/Lanark_County_VHF.yaml
 '
 
-chirp_output_file="tmp/CHIRP-$(date +%Y-%m-%d).csv"
-rt_systems_output_file="tmp/RT-$(date +%Y-%m-%d).csv"
-human_output_file="tmp/HUMAN-$(date +%Y-%m-%d).csv"
-xlsx_output_file="tmp/HUMAN-$(date +%Y-%m-%d).xlsx"
+chirp_csv_file="tmp/CHIRP-${date}.csv"
+rt_systems_csv_file="tmp/RT-${date}.csv"
+human_csv_file="tmp/HUMAN-${date}.csv"
+human_xlsx_file="tmp/HUMAN-${date}.xlsx"
 
 #   ____ _   _ ___ ____  ____
 #  / ___| | | |_ _|  _ \|  _ \
@@ -46,15 +47,15 @@ for input_file in ${input_files}; do
             --modes_allowed AM,FM,NFM  \
             --name_max_length 8        \
             --start_index 1            \
-            > ${chirp_output_file}
+            > ${chirp_csv_file}
     else
         ./scripts/multi_outputter.py   \
             --format CHIRP             \
             --input_file ${input_file} \
             --modes_allowed AM,FM,NFM  \
             --name_max_length 8        \
-            --start_index $(wc -l ${chirp_output_file} | cut -d' ' -f1) \
-            | tail -n '+2' >> ${chirp_output_file}
+            --start_index $(wc -l ${chirp_csv_file} | cut -d' ' -f1) \
+            | tail -n '+2' >> ${chirp_csv_file}
     fi
     index=$((${index} + 1))
 done
@@ -75,14 +76,14 @@ for input_file in ${input_files}; do
             --input_file ${input_file} \
             --modes_allowed AM,FM,NFM  \
             --name_max_length 8        \
-            > ${rt_systems_output_file}
+            > ${rt_systems_csv_file}
     else
         ./scripts/multi_outputter.py   \
             --format RT                \
             --input_file ${input_file} \
             --modes_allowed AM,FM,NFM  \
             --name_max_length 8        \
-            | tail -n '+2' >> ${rt_systems_output_file}
+            | tail -n '+2' >> ${rt_systems_csv_file}
     fi
     index=$((${index} + 1))
 done
@@ -102,18 +103,19 @@ for input_file in ${input_files}; do
             --input_file ${input_file} \
             --modes_allowed AM,FM,NFM  \
             --name_max_length 8        \
-            --start_index 1 > ${human_output_file}
+            --start_index 1 > ${human_csv_file}
     else
         ./scripts/multi_outputter.py   \
             --format HUMAN             \
             --input_file ${input_file} \
             --modes_allowed AM,FM,NFM  \
             --name_max_length 8        \
-            --start_index $(wc -l ${human_output_file} | cut -d' ' -f1) \
-            | tail -n '+2' >> ${human_output_file}
+            --start_index $(wc -l ${human_csv_file} | cut -d' ' -f1) \
+            | tail -n '+2' >> ${human_csv_file}
     fi
     index=$((${index} + 1))
 done
+# Tack on all the digital mode channels at the end
 for input_file in ${input_files}; do
     if [[ 1 == ${index} ]]; then
         ./scripts/multi_outputter.py      \
@@ -121,19 +123,21 @@ for input_file in ${input_files}; do
             --input_file ${input_file}    \
             --modes_allowed DMR,DSTAR,YSF \
             --name_max_length 8           \
-            --start_index 1 > ${human_output_file}
+            --start_index 1 > ${human_csv_file}
     else
         ./scripts/multi_outputter.py      \
             --format HUMAN                \
             --input_file ${input_file}    \
             --modes_allowed DMR,DSTAR,YSF \
             --name_max_length 8           \
-            --start_index $(wc -l ${human_output_file} | cut -d' ' -f1) \
-            | tail -n '+2' >> ${human_output_file}
+            --start_index $(wc -l ${human_csv_file} | cut -d' ' -f1) \
+            | tail -n '+2' >> ${human_csv_file}
     fi
     index=$((${index} + 1))
 done
 
-# Produce pretty PDF handout sheets as a companion to the CHIRP and/or RT Systems output
-./scripts/handouts.py --input_file ${human_output_file} --output_file ${xlsx_output_file}
-libreoffice --headless --convert-to pdf:writer_pdf_Export --outdir $(dirname ${xlsx_output_file}) ${xlsx_output_file}
+# Produce pretty PDF handout sheets as a companion to the CSV output files
+./scripts/handouts.py --input_file ${human_csv_file} \
+    --output_file ${human_xlsx_file}
+libreoffice --headless --convert-to pdf:writer_pdf_Export \
+    --outdir $(dirname ${human_xlsx_file}) ${human_xlsx_file}
