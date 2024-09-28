2ab8a32633a6cb76f2dee78be250327f3749ca9e
Tyler Tidman <tyler.tidman@draak.ca>
2024-09-28 18:47:06 -0400

    Minor tweaks of repeater stuff

--- scripts/generate_rdt.sh
+++ scripts/generate_rdt.sh
@@ -14,7 +14,6 @@ repeaters/Lanark_RLARC.yaml
 repeaters/Ottawa-Gatineau_EMRG.yaml
 repeaters/Ottawa-Gatineau_KARG.yaml
 repeaters/Ottawa-Gatineau_OARC.yaml
-repeaters/Ottawa-Gatineau_OARDG.yaml
 repeaters/Ottawa-Gatineau_OVMRC.yaml
 repeaters/Renfrew_CRRA.yaml
 repeaters/Renfrew_RCARC.yaml
